06d52d142a5564d21e11f6582086baf9ea7ce576
Ben <idben.chen@gmail.com>
2023-09-17 18:08:31 +0800

    first commit

---
+++ client/js/index.js
@@ -0,0 +1,346 @@
+const ws = new WebSocket("ws://localhost:8080");
+const leftArea = document.querySelector(".left .list");
+const leftTitle = document.querySelector(".left h3");
+const rightArea = document.querySelector(".right .list");
+const rightTitle = document.querySelector(".right h3");
+const btnSend = document.querySelector(".btn-send");
+const btnCroom = document.querySelector(".btn-croom");
+const gameArea = document.querySelector(".game");
+const blocks = document.querySelectorAll(".block");
+const lines = document.querySelectorAll(".game .line");
+const userId = new Date().getTime().toString();
+let clientList, targetUserId, roomID, roomList;
+let roomName = "";
+let gameStatus = "none";
+let gameOrder = 0;
+let gameNum = 0;
+leftArea.innerHTML += `<div>已進入聊天室，你的ID是：${userId}</div>`;
+
+btnSend.addEventListener("click", ()=>{
+  sendMessage()
+});
+btnCroom.addEventListener("click", ()=>{
+  createRoom()
+});
+document.querySelector("[name=msg]").addEventListener("keydown", (e)=>{
+  if(e.key === "Enter"){
+    sendMessage()
+  }
+});
+
+blocks.forEach((block, index)=>{
+  block.addEventListener("click", (e)=>{
+    console.log(gameStatus)
+    if(gameStatus === "canPlay"){
+      let prarms = {
+        type: "playingGame",
+        block: index,
+        fromID: userId,
+        roomID,
+        gameOrder
+      }
+      ws.send(JSON.stringify(prarms));
+      gameStatus = "waitting"
+    }
+  })
+});
+
+
+ws.addEventListener("open", () => {
+  console.log("Connected to the WebSocket");
+  let prarms = {
+    type: "register",
+    userId: userId
+  }
+  ws.send(JSON.stringify(prarms));
+});
+
+ws.addEventListener("message", async (event) => {
+  console.log(event)
+  let resutlt = JSON.parse(event.data);
+  if(resutlt.type === "startedGame"){
+    leftArea.innerHTML += `<div><span class="badge bg-success me-1">system</span>人數已滿，遊戲開始。</div>`;
+    if(clientList[0] === userId){
+      leftArea.innerHTML += `<div><span class="badge bg-success me-1">system</span>您為先攻，請選擇第一格</div>`;
+      gameStatus = "canPlay";
+      gameOrder = 0;
+    }else{
+      gameStatus = "waitting";
+      gameOrder = 1;
+    }
+  }
+
+  if(resutlt.type === "setBlock"){
+    [...blocks][resutlt.block].querySelector(".mark"+resutlt.gameOrder).classList.remove("d-none");
+    gameNum++;
+    gameStatus = "waitting";
+    if(userId !== resutlt.fromID){
+      gameStatus = "canPlay";
+      leftArea.innerHTML += `<div><span class="badge bg-success me-1">system</span>對方已下好離手，換你!!</div>`;
+    }
+    return false;
+  }
+
+  if(resutlt.type === "gameOver"){
+    gameStatus = "gameOver";
+    leftArea.innerHTML += `<div><span class="badge bg-success me-1">system</span>遊戲結束!!!</div>`;
+    if(resutlt.winner === -1){
+      leftArea.innerHTML += `<div><span class="badge bg-success me-1">system</span>這局平手</div>`;
+    }else{
+      let winNum = resutlt.winner.winNum;
+      let winLine = resutlt.winner.winLine;
+      if(gameOrder === winNum){
+        leftArea.innerHTML += `<div><span class="badge bg-success me-1">system</span>🏆恭喜你獲勝🏆</div>`;
+      }else{
+        leftArea.innerHTML += `<div><span class="badge bg-success me-1">system</span>很遺憾，這局是你輸了⋯</div>`;
+      }
+      for(let i=0;i<winLine.length;i++){
+        [...lines][winLine[i]].classList.remove("d-none");
+        if(winNum === 0){
+          [...lines][winLine[i]].classList.add("lineColor0");
+        }
+      }
+    }
+    return false;
+  }
+
+  if(resutlt.type === "registered"){
+    roomList = resutlt.allRooms;
+    setRoomList();
+    return false;
+  }
+
+  if(resutlt.type === "joinRoom"){
+    clientList = resutlt.clientList;
+    setClientList();
+    return false;
+  }
+
+  if(resutlt.type === "rejectRoom"){
+    alert(resutlt.message);
+    roomList = resutlt.allRooms;
+    rightTitle.innerHTML = "小房間列表";
+    leftTitle.innerHTML = `聊天室`;
+    leftArea.innerHTML = "";
+    rightArea.innerHTML = "更新中...";
+    btnCroom.innerHTML = "建立房間";
+    btnCroom.classList.remove("btn-danger");
+    roomID = undefined;
+    roomName = undefined;
+    setRoomList();
+    return false;
+  }
+
+  if(resutlt.type === "leaveRoom"){
+    clientList = resutlt.clientList;
+    setClientList();
+    return false;
+  }
+
+  if(resutlt.type === "newRoom"){
+    roomList = resutlt.allRooms;
+    setRoomList();
+    return false;
+  }
+
+  if(resutlt.type === "updateClient"){
+    if(!roomID){
+      return false;
+    }
+    if(roomID !== resutlt.roomID){
+      return false;
+    }
+    clientList = resutlt.otherClients;
+    setClientList();
+  }
+
+  if(resutlt.type === "message"){
+    if(roomID && !resutlt.roomID){
+      return false;
+    }
+    let fromID = resutlt.fromID;
+    let toFix = `<span class="px-2">說</span>`;
+    let icon;
+    if(resutlt.private === true){
+      toFix = `<span class="px-2">對你悄悄說: </span>`;
+    }
+    if(fromID === userId){
+      icon = `<span class="badge bg-primary d-flex align-itmes-center">我自己</span>`
+      if(targetUserId){
+        toFix = `對<span class="badge bg-danger px-2">${targetUserId}</span>悄悄說: `;
+      }else{
+        toFix = `說: `;
+      }
+      
+    }else{
+      if(resutlt.private === true){
+        icon = `<span class="badge bg-danger d-flex align-itmes-center">${fromID}</span>`
+      }else{
+        icon = `<span class="badge bg-primary d-flex align-itmes-center">${fromID}</span>`
+      }
+    }
+    let msg = `<span">${resutlt.message}</span>`;
+    leftArea.innerHTML += `<div class="d-flex align-itmes-center mb-1">${icon}${toFix}${msg}</div>`;
+    scrollToBottom();
+    return false;
+  }
+});
+
+function createRoom(){
+  if(roomID){
+    // alert("將離開小房間");
+    let prarms = {
+      type: "leaveRoom",
+      fromID: userId,
+      roomID: roomID
+    }
+    ws.send(JSON.stringify(prarms));
+    rightTitle.innerHTML = "小房間列表";
+    leftTitle.innerHTML = `聊天室`;
+    leftArea.innerHTML = "";
+    rightArea.innerHTML = "更新中...";
+    btnCroom.innerHTML = "建立房間";
+    btnCroom.classList.remove("btn-danger");
+    gameArea.classList.add("d-none");
+    roomID = undefined;
+    roomName = undefined;
+    return false;
+  }
+  roomID = `room${new Date().getTime().toString()}`;
+  roomName = document.querySelector("[name=roomName]").value;
+  if(roomName === ""){
+    return false;
+  }
+  let prarms = {
+    type: "createRoom",
+    fromID: userId,
+    roomName: roomName,
+    roomID: roomID
+  }
+  ws.send(JSON.stringify(prarms));
+  rightTitle.innerHTML = "使用者列表";
+  leftTitle.innerHTML = `位於聊天室 ${roomName} 中`;
+  leftArea.innerHTML = "";
+  rightArea.innerHTML = "等待加入...";
+  btnCroom.innerHTML = "離開房間";
+  btnCroom.classList.add("btn-danger");
+}
+
+function sendMessage() {
+  var message = document.querySelector("[name=msg]").value;
+  let prarms = {
+    type: "message",
+    message: message,
+    fromID: userId
+  }
+  if(targetUserId){
+    prarms.targetUserId = targetUserId;
+  }
+  if(roomID){
+    prarms.roomID = roomID;
+  }
+  ws.send(JSON.stringify(prarms));
+  console.log(prarms)
+  document.querySelector("[name=msg]").value = "";
+  // if(targetUserId){
+  //   let icon1 = `<span class="badge bg-primary d-flex align-itmes-center pt-1 me-1">我自己</span>`
+  //   let icon2 = `<span class="badge bg-primary d-flex align-itmes-center pt-1 ms-1">${targetUserId}</span>`
+  //   let toFix = `<span class="px-2">悄悄說</span>`;
+  //   let msg = `<span">${message}</span>`;
+  //   leftArea.innerHTML += `<div class="d-flex align-itmes-center mb-1">${icon1}對${icon2}${toFix}：${msg}</div>`;
+  // }
+}
+
+function setRoomList(){
+  if(roomID){
+    return false;
+  }
+  let clientDOM = "";
+  roomList.forEach((clientRoom)=>{
+    let clientRoomID = clientRoom.id;
+    let clientRoomName = clientRoom.name;
+    let clientPeople = parseInt(clientRoom.people);
+    let people2 = "d-inline-block";
+    if(clientPeople == 1){
+      people2 = "d-none";
+    }
+    let dom = `<div roomname="${clientRoomName}" roomid="${clientRoomID}" class="btn btn-secondary w-100 mb-1 text-start"><i class="fa-solid fa-user gameMan man1"></i><i class="fa-solid fa-user gameMan man2 ${people2}"></i>
+        ${clientRoomName}
+      </div>`
+    clientDOM+=dom;
+  });
+  rightArea.innerHTML = clientDOM;
+  let btns = rightArea.querySelectorAll(".btn");
+  btns.forEach(btn=>{
+    btn.addEventListener("click", (e)=>{
+      let roomid = e.currentTarget.getAttribute("roomid");
+      let roomname = e.currentTarget.getAttribute("roomname");
+      roomID = roomid;
+      let prarms = {
+        type: "joinRoom",
+        fromID: userId,
+        roomID: roomID
+      }
+      ws.send(JSON.stringify(prarms));
+      rightTitle.innerHTML = "使用者列表";
+      leftTitle.innerHTML = `位於聊天室 ${roomname} 中`;
+      leftArea.innerHTML = "";
+      rightArea.innerHTML = "更新中...";
+      btnCroom.innerHTML = "離開房間";
+      btnCroom.classList.add("btn-danger");
+    })
+  })
+}
+
+function setClientList(){
+  console.log(clientList)
+  clientDOM = "";
+  clientList.forEach((client)=>{
+    if(client !== userId){
+      let dom = `<div idn="${client}" class="btn btn-secondary w-100 mb-1">${client}</div>`
+      clientDOM+=dom;
+    }
+  });
+  rightArea.innerHTML = clientDOM;
+  let btns = rightArea.querySelectorAll(".btn");
+  btns.forEach(btn=>{
+    btn.addEventListener("click", (e)=>{
+      let target = e.currentTarget;
+      let idn = e.currentTarget.getAttribute("idn");
+      if(target.classList.contains("btn-danger")){
+        target.classList.remove("btn-danger");
+        targetUserId = undefined;
+      }else{
+        target.classList.add("btn-danger");
+        targetUserId = idn;
+      }
+    })
+  })
+  if(clientList.length >= 2){
+    gameArea.classList.remove("d-none");
+    blocks.forEach(block=>{
+      block.querySelector(".mark0").classList.add("d-none");
+      block.querySelector(".mark1").classList.add("d-none");
+    });
+    lines.forEach(line=>{
+      line.classList.add("d-none");
+    });
+    if(clientList[0] === userId){
+      let prarms = {
+        type: "startGame",
+        fromID: userId,
+        roomID
+      }
+      ws.send(JSON.stringify(prarms));
+    }
+  }else{
+    gameArea.classList.add("d-none");
+  }
+  
+}
+
+function scrollToBottom() {
+  leftArea.scrollTop = leftArea.scrollHeight - leftArea.clientHeight;
+}
+
+
---
+++ server/index.js
@@ -0,0 +1,256 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+// 用於存儲客戶端的使用者列表
+const clients = {};
+const rooms = {};
+
+wss.on("connection", (connection) => {
+  console.log("新使用者已經連線");
+
+  connection.on("message", (message) => {
+    const parsedMessage = JSON.parse(message);
+    console.log(`收到訊息 => ${message}`);
+    if (parsedMessage.type === "register") {
+      const userId = parsedMessage.userId;
+      clients[userId] = connection;
+      connection.userId = userId;
+
+      const otherClients = Object.keys(clients);
+      let allRooms = [];
+      for (const [key, value] of Object.entries(rooms)) {
+        let id = key;
+        let name = value.name;
+        let people = value.userList.length;
+        allRooms.push({id, name, people});
+      }
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: "registered", otherClients, allRooms }));
+        }
+      });
+
+      return;
+    }
+    
+    if(parsedMessage.type === "createRoom"){
+      let roomID = parsedMessage.roomID;
+      rooms[roomID] = {
+        id: parsedMessage.roomID,
+        name: parsedMessage.roomName
+      }
+      rooms[roomID].userList = [];
+      rooms[roomID].userList.push(parsedMessage.fromID);
+      let allRooms = [];
+      for (const [key, value] of Object.entries(rooms)) {
+        let id = key;
+        let name = value.name;
+        let people = value.userList.length;
+        allRooms.push({id, name, people});
+      }
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: "newRoom", allRooms }));
+        }
+      });
+      return;
+    }
+
+    if(parsedMessage.type === "joinRoom"){
+      let roomID = parsedMessage.roomID;
+      let fromID = parsedMessage.fromID;
+      if(rooms[roomID].userList.length >= 2){
+        const targetClient = clients[fromID];
+        let allRooms = [];
+        for (const [key, value] of Object.entries(rooms)) {
+          let id = key;
+          let name = value.name;
+          let people = value.userList.length;
+          allRooms.push({id, name, people});
+        }
+        if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+          targetClient.send(JSON.stringify({ type: "rejectRoom", message: "人數已滿", allRooms}));
+        }
+        return false;
+      }
+      rooms[roomID].userList.push(fromID);
+      let clientList = rooms[roomID].userList;
+      rooms[roomID].userList.forEach(userID=>{
+        const targetClient = clients[userID];
+        if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+          targetClient.send(JSON.stringify({ type: "joinRoom", fromID, roomID, clientList}));
+        }
+      });
+      return;
+    }
+
+    if(parsedMessage.type === "leaveRoom"){
+      let roomID = parsedMessage.roomID;
+      let fromID = parsedMessage.fromID;
+      rooms[roomID].userList = arrayRemove(rooms[roomID].userList , fromID)
+      let clientList = rooms[roomID].userList;
+      rooms[roomID].userList.forEach(userID=>{
+        const targetClient = clients[userID];
+        if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+          targetClient.send(JSON.stringify({ type: "leaveRoom", fromID, roomID, clientList}));
+        }
+      });
+      if(rooms[roomID].userList.length === 0){
+        delete rooms[roomID];
+      }
+      let allRooms = [];
+      for (const [key, value] of Object.entries(rooms)) {
+        let id = key;
+        let name = value.name;
+        let people = value.userList.length;
+        allRooms.push({id, name, people});
+      }
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: "newRoom", allRooms }));
+        }
+      });
+      return;
+    }
+
+    if(parsedMessage.type === "startGame"){
+      let roomID = parsedMessage.roomID;
+      let fromID = parsedMessage.fromID;
+      rooms[roomID].boardAry = [];
+      for(let i=0;i<9;i++) {
+        rooms[roomID].boardAry.push(-1);
+      }
+      let clientList = rooms[roomID].userList;
+      clientList.forEach(userID=>{
+        const targetClient = clients[userID];
+        if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+          targetClient.send(JSON.stringify({ type: "startedGame", fromID, roomID, clientList}));
+        }
+      });
+    }
+
+    if(parsedMessage.type === "playingGame"){
+      let roomID = parsedMessage.roomID;
+      let fromID = parsedMessage.fromID;
+      let block = parsedMessage.block;
+      let gameOrder = parsedMessage.gameOrder;
+      rooms[roomID].boardAry[block] = gameOrder;
+      let clientList = rooms[roomID].userList;
+      clientList.forEach(userID=>{
+        const targetClient = clients[userID];
+        if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+          targetClient.send(JSON.stringify({ type: "setBlock", fromID, roomID,block,gameOrder, clientList}));
+        }
+      });
+      let winner = checkWinner(rooms[roomID].boardAry);
+      if(winner !== -1){
+        clientList.forEach(userID=>{
+          const targetClient = clients[userID];
+          console.log(winner);
+          if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+            targetClient.send(JSON.stringify({ type: "gameOver", fromID, roomID, winner, clientList}));
+          }
+        });
+        return false;
+      }
+      if(rooms[roomID].boardAry.length >= 9){
+        let gameOverCheck = true;
+        rooms[roomID].boardAry.forEach((bNum, index) => {
+          if(bNum === -1){
+            gameOverCheck = false;
+          }
+        })
+        if(gameOverCheck === true){
+          winner = checkWinner(rooms[roomID].boardAry);
+          clientList.forEach(userID=>{
+            const targetClient = clients[userID];
+            if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+              targetClient.send(JSON.stringify({ type: "gameOver", fromID, roomID,winner, clientList}));
+            }
+          });
+        } 
+      }
+      return;
+    }
+    
+    if (parsedMessage.type === "message") {
+      const targetUserId = parsedMessage.targetUserId;
+      const fromID = parsedMessage.fromID;
+      const roomID = parsedMessage.roomID;
+      if(roomID){
+        if(targetUserId){
+          wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify({ type: "message", "message": parsedMessage.message, fromID, roomID, targetUserId, private: true }));
+            }
+          });
+        }else{
+          rooms[roomID].userList.forEach(userID=>{
+            const targetClient = clients[userID];
+            if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+              targetClient.send(JSON.stringify({ type: "message", message: parsedMessage.message, fromID, roomID}));
+            }
+          });
+        }
+        return false;
+      }
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: "message", "message": parsedMessage.message, fromID: fromID }));
+        }
+      });
+    }
+  });
+  
+
+  connection.on("close", () => {
+    console.log("已經用者斷開連線");
+    let dsID = connection.userId;
+    // 從客戶端列表中移除
+    if (connection.userId) {
+      delete clients[connection.userId];
+    }
+    const otherClients = Object.keys(clients);
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: "updateClient", otherClients , disconnectedID: dsID}));
+      }
+    });
+  });
+});
+
+function checkWinner(boardAry) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  let winLine = [];
+  let winNum;
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (boardAry[a] !== -1 && boardAry[a] === boardAry[b] && boardAry[a] === boardAry[c]) {
+      winNum = boardAry[a];
+      winLine.push(i)
+    }
+  }
+  if(winLine.length === 0){
+    return -1;
+  }
+  return {winNum, winLine}; 
+}
+
+function arrayRemove(arr, value) {
+
+  return arr.filter(function (geeks) {
+      return geeks != value;
+  });
+
+}
